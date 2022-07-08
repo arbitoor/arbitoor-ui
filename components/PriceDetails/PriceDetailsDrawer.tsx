1e6ab5c3956f797f1d799d1bd211f05afc389767
skiran017 <skiran017@gmail.com>
2022-07-08 14:52:14 +0530

    fix (#50)
    -conditionally hide and show price details button and drawer based on route
    -fix tx input value

--- components/PriceDetails/PriceDetailsDrawer.tsx
+++ components/PriceDetails/PriceDetailsDrawer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, chakra, Button, Flex, Text } from '@chakra-ui/react';
 import { useGlobalStore } from '../../utils/globalStore';
 import { percentToNumber } from '../../utils/helpers';
+import { toPrecision } from '@arbitoor/arbitoor-core';
 
 function PriceDetailsDrawer() {
   const paths = useGlobalStore((state) => state.paths);
@@ -10,57 +11,64 @@ function PriceDetailsDrawer() {
   const minReceivedAmount = +paths[0]?.output - percentToNumber(slippageValue);
 
   return (
-    <Flex
-      direction="column"
-      color="whitesmoke"
-      width="100%"
-      bg="#26262C"
-      height="148px"
-      borderRadius="12px"
-      mt="4"
-    >
-      <Flex
-        border="1px solid"
-        borderColor=" #FF3260"
-        borderRadius="12px"
-        height="100%"
-        m="12px"
-        p="12px"
-        lineHeight="1.75"
-        direction="column"
-        fontSize="14px"
-        letterSpacing="0.5px"
-      >
-        {/* <Text>Slippage tolerance : 0.5 %</Text> */}
-        <Text>
-          Minimum Received :{' '}
-          {paths.length
-            ? minReceivedAmount > 0
-              ? minReceivedAmount.toFixed(4)
-              : '--'
-            : '--'}
-        </Text>
-        <Box display="grid" gridTemplateColumns="0.5fr 0.5fr 2fr">
-          <Text flex="1 0 0%">Routes :</Text>
-          <Flex direction="column" alignItems="center">
-            {paths.length
-              ? paths[0].routePercentage?.map((pecent: string, idx: number) => {
-                  return <Text key={idx}>{pecent}%</Text>;
-                })
-              : '--'}
-          </Flex>
-          <Flex direction="row">
-            <Box flex={['1 0 45%', '1 0 55%', '1 0 65%', '1 0 65%']}>
+    <>
+      {paths && paths[0]?.path?.length ? (
+        <Flex
+          direction="column"
+          color="whitesmoke"
+          width="100%"
+          bg="#26262C"
+          height="148px"
+          borderRadius="12px"
+          mt="4"
+        >
+          <Flex
+            border="1px solid"
+            borderColor=" #FF3260"
+            borderRadius="12px"
+            height="100%"
+            m="12px"
+            p="12px"
+            lineHeight="1.75"
+            direction="column"
+            fontSize="14px"
+            letterSpacing="0.5px"
+          >
+            {/* <Text>Slippage tolerance : 0.5 %</Text> */}
+
+            <Text>
+              Minimum Received :{' '}
               {paths.length
-                ? paths[0].path?.map((ticker: string, idx: number) => {
-                    return <Text key={idx}>{ticker}</Text>;
-                  })
+                ? minReceivedAmount > 0
+                  ? toPrecision(minReceivedAmount.toString(), 4)
+                  : '--'
                 : '--'}
+            </Text>
+            <Box display="grid" gridTemplateColumns="0.5fr 0.5fr 2fr">
+              <Text flex="1 0 0%">Routes :</Text>
+              <Flex direction="column" alignItems="center">
+                {paths.length
+                  ? paths[0].routePercentage?.map(
+                      (pecent: string, idx: number) => {
+                        return <Text key={idx}>{pecent}%</Text>;
+                      }
+                    )
+                  : '--'}
+              </Flex>
+              <Flex direction="row">
+                <Box flex={['1 0 45%', '1 0 55%', '1 0 65%', '1 0 65%']}>
+                  {paths.length
+                    ? paths[0].path?.map((ticker: string, idx: number) => {
+                        return <Text key={idx}>{ticker}</Text>;
+                      })
+                    : '--'}
+                </Box>
+              </Flex>
             </Box>
           </Flex>
-        </Box>
-      </Flex>
-    </Flex>
+        </Flex>
+      ) : null}
+    </>
   );
 }
 
