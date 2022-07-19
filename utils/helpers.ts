cfd8866467282e684c338dd13352ff97bdd50e94
skiran017 <skiran017@gmail.com>
2022-07-19 13:01:59 +0530

    feat: to show token balances in the token list modal
    todo: needs refactor

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,6 @@
+import Big from 'big.js'
+import { Account } from 'near-api-js';
+
 export function debounce(cb: any, delay: any) {
   let timer: any;
   return function (...args: any) {
@@ -11,3 +14,20 @@ export function debounce(cb: any, delay: any) {
 export function percentToNumber(value: any) {
   return (value / 100);
 }
+
+/**
+ * Get balance of the account as a BN
+ */
+export const ftBalanceOf = async (
+  account: Account,
+  tokenId: string,
+  accountId: string
+) => {
+  return account
+    .viewFunction(tokenId, 'ft_balance_of', { account_id: accountId })
+    .then((n) => new Big(n))
+    .catch(() => {
+      return new Big(0);
+    });
+};
+
