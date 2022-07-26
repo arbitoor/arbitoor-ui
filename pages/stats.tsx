bedc3424a0785dc2c2d1bdba0170a46e52832e86
skiran017 <skiran017@gmail.com>
2022-07-26 10:44:06 +0530

    feat: recharts and mock stats data init

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+import { statsData } from '../utils/mockStatsData';
 
 function stats() {
+  const sortedData = [];
+
+  for (const data of statsData) {
+    const dexName = data.dex;
+    console.log(data);
+
+    // sortedData.push(data);
+  }
+
+  const dexCount = statsData.filter((data) => {
+    console.log(data.dex);
+  });
+
+  // console.log(sortedData);
   return (
     <div
       style={{
@@ -11,7 +37,27 @@ function stats() {
         alignItems: 'center',
       }}
     >
-      Stats Coming soon....
+      {/* <ResponsiveContainer width="100%" height="100%"> */}
+      <BarChart
+        width={500}
+        height={300}
+        data={statsData}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="dexName" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="dexCount" fill="#8884d8" />
+        {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+      </BarChart>
+      {/* </ResponsiveContainer> */}
     </div>
   );
 }
---
+++ utils/mockStatsData.ts
@@ -0,0 +1,54 @@
+export const statsData = [
+  {
+    "receipt_id": "t8Wa4xP9WNYymTyMqMs6Mk2C3WZneiAyCYvQizC6x8o",
+    "block_height": "68893936",
+    "blocktime": "1656666875806",
+    "dex": "v2.ref-finance.near",
+    "sender": "spoiler.near",
+    "success": true,
+    "amount_in": "3686441724015376000000000",
+    "amount_out": "3871426512596908500",
+    "pool_id": "3449",
+    "token_in": "wrap.near",
+    "token_out": "token.pembrock.near"
+  },
+  {
+    "receipt_id": "t8Wa4xP9WNYymTyMqMs6Mk2C3WZneiAyCYvQizC6x8o",
+    "block_height": "68893936",
+    "blocktime": "1656666875806",
+    "dex": "v2.ref-finance.near",
+    "sender": "spoiler.near",
+    "success": true,
+    "amount_in": "3871426512596908500",
+    "amount_out": "2985207570660297000",
+    "pool_id": "3448",
+    "token_in": "token.pembrock.near",
+    "token_out": "usn"
+  },
+  {
+    "receipt_id": "t8Wa4xP9WNYymTyMqMs6Mk2C3WZneiAyCYvQizC6x8o",
+    "block_height": "68893936",
+    "blocktime": "1656666875806",
+    "dex": "v2.ref-finance.near",
+    "sender": "spoiler.near",
+    "success": true,
+    "amount_in": "433558275984624100000000",
+    "amount_out": "351078907462545860",
+    "pool_id": "3269",
+    "token_in": "wrap.near",
+    "token_out": "usn"
+  },
+  {
+    "receipt_id": "KLxxUhHQZm4H36DqeBRSpcqDTqE18XWAfifdScoRNoL",
+    "block_height": "70599303",
+    "blocktime": "1658734740685",
+    "dex": "v1.orderbook.near",
+    "sender": "spoiler.near",
+    "success": true,
+    "amount_in": "1000999999999999900",
+    "amount_out": "1000500.5009999999",
+    "pool_id": "J5mggeEGCyXVUibvYTe9ydVBrELECRUu23VRk2TwC2is",
+    "token_in": "usn",
+    "token_out": "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near"
+  }
+]
